2436ff4c0a204a33bc173cd3cc04c05060c60891
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-03-09 14:54:54 +0100

    Remove file duplication and use if/else instead depending on the qsfp port variable

--- ip/u280/ethernet-u280.tcl
+++ ip/u280/ethernet-u280.tcl
@@ -34,6 +34,30 @@ set files [list \
   [file normalize ${g_root_dir}/src/verilog-ethernet/syn/vivado/eth_mac_fifo.tcl] \
 ]
 
+# QSFP Port 0
+if { $g_qsfp_port == "qsfp1" } {
+create_ip -name gtwizard_ultrascale -vendor xilinx.com -library ip -module_name gtwizard_ultrascale_0
+
+set_property -dict [list CONFIG.preset {GTY-10GBASE-R}] [get_ips gtwizard_ultrascale_0]
+
+set_property -dict [list \
+    CONFIG.CHANNEL_ENABLE {X0Y40} \
+    CONFIG.TX_MASTER_CHANNEL {X0Y40} \
+    CONFIG.RX_MASTER_CHANNEL {X0Y40} \
+    CONFIG.TX_LINE_RATE {10.3125} \
+    CONFIG.TX_REFCLK_FREQUENCY {161.1328125} \
+    CONFIG.TX_USER_DATA_WIDTH {64} \
+    CONFIG.TX_INT_DATA_WIDTH {64} \
+    CONFIG.RX_LINE_RATE {10.3125} \
+    CONFIG.RX_REFCLK_FREQUENCY {161.1328125} \
+    CONFIG.RX_USER_DATA_WIDTH {64} \
+    CONFIG.RX_INT_DATA_WIDTH {64} \
+    CONFIG.RX_REFCLK_SOURCE {X0Y40 clk1} \
+    CONFIG.TX_REFCLK_SOURCE {X0Y40 clk1} \
+    CONFIG.FREERUN_FREQUENCY {125} \
+] [get_ips gtwizard_ultrascale_0]
+
+} else { 
 
 # QSFP Port 1
 create_ip -name gtwizard_ultrascale -vendor xilinx.com -library ip -module_name gtwizard_ultrascale_1
@@ -56,3 +80,5 @@ set_property -dict [list CONFIG.preset {GTY-10GBASE-R}] [get_ips gtwizard_ultras
      CONFIG.TX_REFCLK_SOURCE {X0Y44 clk1} \
      CONFIG.FREERUN_FREQUENCY {125} \
  ] [get_ips gtwizard_ultrascale_1]
+
+}
--- tcl/project_options.tcl
+++ tcl/project_options.tcl
@@ -1,3 +1,3 @@
 
 
-source ${g_root_dir}/ip/${g_board_part}/ethernet-${g_board_part}-${g_qsfp_port}.tcl
+source ${g_root_dir}/ip/${g_board_part}/ethernet-${g_board_part}.tcl
